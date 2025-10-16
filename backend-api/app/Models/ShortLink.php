1a43cbf2d409d3ca681075290c467ce6ed1804ef
fredotcom24 <pro.fredseo@gmail.com>
2025-10-16 17:37:52 +0100

    [feat]: create model for short links

---
+++ backend-api/app/Models/ShortLink.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ShortLink extends Model
+{
+    /** @use HasFactory<\Database\Factories\ShortLinkFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'original_url',
+        'code',
+        'clicks',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'clicks' => 'boolean',
+        ];
+    }
+
+}
