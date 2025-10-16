a12cbb4e64c3766b75aead00bab52665ff3ef8e7
fredotcom24 <pro.fredseo@gmail.com>
2025-10-16 15:04:52 +0100

    [feat]: create module controller

---
+++ backend-api/app/Http/Controllers/ModuleController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreModulesRequest;
+use App\Http\Requests\UpdateModulesRequest;
+use App\Models\Module;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ModuleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = DB::table('modules')->get(['id', 'name', 'description']);
+
+        return response()->json([
+            $data
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Module $modules)
+    {
+        //
+    }
+}
