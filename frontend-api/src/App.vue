c9ed548a9f9ffbeab0e8fc8de536a2c00817cc9f
fredotcom24 <pro.fredseo@gmail.com>
2025-10-17 11:28:16 +0100

    [build]: create vue js frontend project

---
+++ frontend-api/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+// import { AuthModel } from './services/auth'
+
+// const authStore = AuthModel()
+
+//     const data = {
+//         email: 'sama@gmail.com',
+//         password: 'azertyui',
+//     }
+
+//     const response = await authStore.login(data)
+//     console.log(response.data)
+</script>
+
+<template>
+<RouterView />
+</template>
+
+<style scoped></style>
