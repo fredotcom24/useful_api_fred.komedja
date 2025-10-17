c0e16ce0d360ac631180fbeb151b5712a111b7c9
fredotcom24 <pro.fredseo@gmail.com>
2025-10-17 12:41:42 +0100

    [feat]: create show modules view

---
+++ frontend-api/src/views/modules/ShowModule.vue
@@ -0,0 +1,53 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Activate</th>
+                    <th>Deactivate</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr v-for="module in modules" :key="module.id">
+                    <td>{{ module.name || 'N/A' }}</td>
+                    <td>{{ module.description || 'N/A' }}</td>
+                    <td><button @click="activateModule(module.id)">Activate</button></td>
+                    <td><button @click="deactivateModule(module.id)">Deactivate</button></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script setup>
+import {ModuleStore} from '@/stores/moduleStore';
+import {ref, onMounted} from 'vue';
+
+const moduleStore = ModuleStore();
+const modules = ref([])
+
+const loadModules = async () => {
+    try {
+        await moduleStore.getAllModules()
+        modules.value = moduleStore.modules
+    } catch(error) {
+        console.error('Error login', error);
+    }
+}
+
+const activateModule = async (id) => {
+    await moduleStore.activateModule(id)
+}
+
+const deactivateModule = async (id) => {
+    await moduleStore.deactivateModule(id)
+}
+
+onMounted(()=> {
+    loadModules()
+})
+</script>
