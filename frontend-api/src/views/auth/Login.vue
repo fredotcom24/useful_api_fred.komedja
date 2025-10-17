e0962ed45778a21839b400b9e72a2eb9dbaf490a
fredotcom24 <pro.fredseo@gmail.com>
2025-10-17 11:30:54 +0100

    [feat]: create account login view

---
+++ frontend-api/src/views/auth/Login.vue
@@ -0,0 +1,40 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+        <label>Email</label>
+        <input v-model="email" type="email" placeholder="tonton@email.co"> <br>
+        <label >Password</label>
+        <input v-model="password" type="password" placeholder="********"> <br>
+        <button @click="handleLogin" type="button">
+            Login
+        </button>
+
+        <div>
+            <p>I don't have account</p>
+            <router-link to="/register">Register here</router-link>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { AuthStore } from '@/stores/authStore';
+import { ref } from 'vue';
+
+const authStore = AuthStore();
+const email = ref('')
+const password = ref('')
+
+const handleLogin = async () => {
+
+    const data = {
+        email: email.value,
+        password: password.value,
+    }
+
+    try {
+        await authStore.login(data)
+    } catch (error) {
+        console.error('Error login', error);   
+    }
+}
+</script>
